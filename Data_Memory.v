e3300c0a980c3d553fef3c5254cce8ec106e2eb7
poojith06 <saipoojith2006@gmail.com>
2026-02-19 15:57:32 +0530

    Update Data_Memory module

--- Data_Memory.v
+++ Data_Memory.v
@@ -14,18 +14,18 @@ module Data_Memory(input clk,reset,MemRead,MemWrite, input[63:0] address,write_d
                 memory[k] <= 8'h0;
         end else begin
             if (MemWrite) begin
-                memory[address]   <= write_data[7:0];
-                memory[address+1] <= write_data[15:8];
-                memory[address+2] <= write_data[23:16];
-                memory[address+3] <= write_data[31:24];
-                memory[address+4] <= write_data[39:32];
-                memory[address+5] <= write_data[47:40];
-                memory[address+6] <= write_data[55:48];
-                memory[address+7] <= write_data[63:56];
+                memory[address]     <= write_data[63:56];
+                memory[address + 1] <= write_data[55:48];
+                memory[address + 2] <= write_data[47:40];
+                memory[address + 3] <= write_data[39:32];
+                memory[address + 4] <= write_data[31:24];
+                memory[address + 5] <= write_data[23:16];
+                memory[address + 6] <= write_data[15:8];
+                memory[address + 7] <= write_data[7:0];
             end
         end
     end
 
-    assign read_data = MemRead ? {memory[address+7],memory[address+6],memory[address+5],memory[address+4],memory[address+3],memory[address+2],memory[address+1],memory[address]} : 64'b0;
+    assign read_data = MemRead ? {memory[address],memory[address+1],memory[address+2],memory[address+3],memory[address+4],memory[address+5],memory[address+6],memory[address+7]} : 64'b0;
 
 endmodule
