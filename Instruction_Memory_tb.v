b9ffdc32ea4f8ba2deead026d0f4c752ca0c10be
poojith06 <saipoojith2006@gmail.com>
2026-02-20 21:50:18 +0530

    Refactor Instruction_Memory and its testbench by removing unused clk and reset signals

--- Instruction_Memory_tb.v
+++ Instruction_Memory_tb.v
@@ -3,37 +3,24 @@
 
 module Instruction_Memory_tb;
 
-    reg clk;
-    reg reset;
     reg [63:0] addr;
     wire [31:0] instr;
 
     Instruction_Memory uut(
-        .clk(clk),
-        .reset(reset),
         .addr(addr),
         .instr(instr)
     );
 
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk;
-    end
 
     task run_test;
         input [4:0]  tno;
-        input        t_reset;
         input [63:0] t_addr;
         input [31:0] expected;
         begin
-            @(negedge clk);
-            reset = t_reset;
             addr  = t_addr;
-
             #6;
 
             $display("Test %0d", tno);
-            $display("Reset       = %b", reset);
             $display("Address     = %0d", addr);
             $display("Instruction = %h", instr);
 
@@ -50,10 +37,10 @@ module Instruction_Memory_tb;
         $dumpfile("Instruction_Memory_tb.vcd");
         $dumpvars(0, Instruction_Memory_tb);
 
-        run_test(1, 1'b0, 64'd0,  32'h00500113);
-        run_test(2, 1'b0, 64'd4,  32'h00A00193);
-        run_test(3, 1'b0, 64'd8,  32'h003100B3);
-        run_test(4, 1'b0, 64'd12, 32'h40310133);
+        run_test(1, 64'd0,  32'h00500113);
+        run_test(2, 64'd4,  32'h00A00193);
+        run_test(3, 64'd8,  32'h003100B3);
+        run_test(4, 64'd12, 32'h40310133);
 
         #20 $finish;
     end
